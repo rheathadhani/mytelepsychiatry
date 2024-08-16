ac495929026984e8bdbb1bee6ef4781dea5859b3
rheathadhani <rheathadhani@gmail.com>
2024-08-16 20:40:05 +0800

    1608

--- client/patient-index.js
+++ client/patient-index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#profile-link').click(function(event) {
       event.preventDefault(); // Prevent the default link behavior
-      $('#page-content-wrapper').load('profile.html'); // Load the profile content into the page content wrapper
+      $('#page-content-wrapper').load('patient-index.html'); // Load the profile content into the page content wrapper
     });
   });
   
